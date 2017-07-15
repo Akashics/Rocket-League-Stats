78694d8a27fd3898d7182c1529dabef924a518b9
Kashall <Kashalls@users.noreply.github.com>
2017-07-15 16:20:16 -0700

    Added RLS-API v1

---
+++ commands/search.js
@@ -0,0 +1,80 @@
+exports.name = "search";
+exports.run = (client, message, args) => {
+
+    const config = require('../config.json');
+    const rls = require('rls-api');
+    const request = require('request');
+
+    const help = `You can use the command like this: r!search <platform> <user>`;
+
+    var rlsstats = new rls.Client({
+        token: `${config.rocketLeagueStatsKey}`
+    });
+
+    if (args[0] === 'steam' || args[0] === 'ps4' || args[0] === 'xbox') {
+        if (args[0] === 'steam') {
+            if (!args[1]) return message.channel.send(help);
+            let username = isNaN(args[1]);
+            if (username == false) {
+                try {
+                    message.channel.send({ files: ["http://signature.rocketleaguestats.com/normal/steam/" + args[1] + ".png"] });
+                    return message.channel.stopTyping();
+                } catch (e) {
+                    console.log(e)
+                    message.channel.send({ files: ["http://signature.rocketleaguestats.com/normal/steam/0.png"] });
+                    message.channel.stopTyping();
+                    return message.reply(`:x: An error has occured: ${e}`);
+                }
+
+            } else {
+                try {
+                    message.channel.startTyping();
+                    request('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + config.steamKey + '&vanityurl=' + args[1], (error, response, body) => {
+                        if (!error && response.statusCode === 200) {
+                            const steamID64 = JSON.parse(body).response.steamid;
+                            try {
+                                message.channel.startTyping();
+                                message.channel.send({ files: ["http://signature.rocketleaguestats.com/normal/steam/" + steamID64 + ".png"] });
+                                return message.channel.stopTyping();
+                            } catch (e) {
+                                message.channel.send({ files: ["http://signature.rocketleaguestats.com/normal/steam/0.png"] });
+                                message.channel.stopTyping();
+                                return message.reply(`:x: An error has occured: ${e}`);
+                            }
+                        }
+                    });
+                } catch (e) {
+                    return message.reply(`:x: An error has occured: ${e}`);
+                }
+            }
+        }
+        if (args[0] === 'ps4') {
+            if (!args[1]) return message.channel.send(help);
+            try {
+                message.channel.startTyping();
+                message.channel.send({ files: ["http://signature.rocketleaguestats.com/normal/ps4/" + args[1] + ".png"] });
+                return message.channel.stopTyping();
+            } catch (e) {
+                message.channel.send({ files: ["http://signature.rocketleaguestats.com/normal/ps4/0.png"] });
+                message.channel.stopTyping();
+                return message.reply(`:x: An error has occured: ${e}`);
+            }
+        }
+        if (args[0] === 'xbox') {
+            if (!args[1]) return message.channel.send(help);
+            try {
+                message.channel.startTyping();
+                message.channel.send('The API we use to receive data for gamertags is a bit slow. This will take a little bit.');
+                message.channel.send({ files: ["http://signature.rocketleaguestats.com/normal/xboxone/" + args[1] + ".png"] });
+                return message.channel.stopTyping();
+            } catch (e) {
+                message.channel.send({ files: ["http://signature.rocketleaguestats.com/normal/xboxone/0.png"] });
+                message.channel.stopTyping();
+                return message.reply(`:x: An error has occured: ${e}`);
+            }
+        }
+    } else {
+        return message.channel.send(help);
+    }
+
+}
--- commands/stats.js
+++ commands/stats.js
@@ -2,48 +2,36 @@ exports.name = "stats";
 exports.run = (client, message, args) => {
 
     const config = require('../config.json');
+    const rls = require('rls-api');
     const request = require('request');
-    if (!args) {
-        return message.channel.send('Please refer back to the help document.');
-    }
-    if (args[0] === 'profile' || args[0] === 'id') {
-        if (args[0] == "profile") {
-            if (args[1].length > 1) {
-                message.channel.startTyping();
-                request('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + config.steamKey + '&vanityurl=' + args[1], (error, response, body) => {
-                    if (!error && response.statusCode === 200) {
-                        const steamID64 = JSON.parse(body).response.steamid;
-                        console.log(steamID64)
-                        try {
-                            message.channel.send({ files: ["http://signature.rocketleaguestats.com/normal/steam/" + steamID64 + ".png"] });
-                            return message.channel.stopTyping();
-                        } catch (e) {
-                            console.log(e)
-                            message.channel.stopTyping();
-                            return message.reply(`:x: An error has occured: ${e}`);
-                        }
-                    }
-                });
-            } else {
-                return message.channel.send('Please provide me a profile name to lookup.');
-            }
+    //Lets start the rlsstats
+    var rlsstats = new rls.Client({
+        token: `${config.rocketLeagueStatsKey}`
+    });
+
+    /*rlsstats.getPlaylistsData(function (status, data) {
+        if (status == 200) {
+            console.log("-- Playlists data:");
+            console.log(data);
         }
-        if (args[0] == "id") {
-            if (args[1].length = 17) {
-                try {
-                    message.channel.startTyping();
-                    message.channel.send({ files: ["http://signature.rocketleaguestats.com/normal/steam/" + args[1] + ".png"] });
-                    return message.channel.stopTyping();
-                } catch (e) {
-                    console.log(e)
-                    message.channel.stopTyping();
-                    return message.reply(`:x: An error has occured: ${e}`);
-                }
-            } else {
-                return message.channel.send('You need to have 17-digit ID Number to provide me with.');
-            }
+    });
+
+    rlsstats.getTiersData(function (status, data) {
+        if (status == 200) {
+            console.log("-- Tiers data:");
+            console.log(data);
         }
-    } else {
-        return message.channel.send(`That didn't seem right, why don't you try again?`);
-    }
+    });
+
+    rlsstats.getRankedLeaderboard(rls.rankedPlaylists.DUEL, function (status, data) {
+        if (status == 200) {
+            console.log(data)
+        }
+    });*/
+
+    rlsstats.getStatLeaderboard(rls.statType.GOALS, function (status, data) {
+        if (status == 200) {
+            console.log(data);
+        }
+    });
 }
